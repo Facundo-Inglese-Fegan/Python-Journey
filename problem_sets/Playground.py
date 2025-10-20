a74919281706e1394ac31c2281fd77e0b8ec5ca5
Facundo Inglese Fegan <facuinglesefegan@gmail.com>
2025-10-20 14:12:49 +0200

    feat: Modify Playground.py

--- problem_sets/Playground.py
+++ problem_sets/Playground.py
@@ -1,12 +1,13 @@
+# This is a simple playground for testing code snippets.
+
 #%%
 import random
 
 message = "¡Jugando con código!"
 print(message)
 
-#%%
+# %%
 number = random.randint(1, 100)
 print(f"Mi número aleatorio es: {number}")
 # %%
 print("Fin del Playground.py")
-# %%
